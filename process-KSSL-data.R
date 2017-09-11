e79b70e02f565d01632a89d73730c16d967b820a
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-09-11 14:45:48 -0700

    site-level records are no longer extracted from the KSSL/LIMS snapshot, only IDs are used to later link with NASIS records

--- process-KSSL-data.R
+++ process-KSSL-data.R
@@ -1,23 +1,16 @@
-## TODO: convert this to use the new SQLite DB via Adolfo's FGDB
+## Get lab characterization data from LIMS snapshot; morphologic data are from NASIS
+##
+## D.E. Beaudette
+##
+## 2017-09-11: major re-write
+##
 
 
-## get site data from the KSSL snapshot
-## 2015-11-23: there are some cases of multiple pedons / site, but with multiple user site IDs (see pedon_key = 28794)
-##             removing user site ID and keeping unique set of rows
-## 2015-11-23: MLRA, SSA, state codes can't be trusted and are leading to duplicates -> removed them from join to NCSS_Site_Location
-q.site <- "SELECT
-pedon_key, pedlabsampnum, upedonid AS pedon_id,
-NCSS_Pedon_Taxonomy.longitude_decimal_degrees as x, NCSS_Pedon_Taxonomy.latitude_decimal_degrees as y, 
-samp_name AS sampled_as, corr_name AS correlated_as,
-corr_class_type as correlated_taxon_kind,
-cntrl_depth_to_top as pscs_top, cntrl_depth_to_bot as pscs_bottom, pedon_completeness_index
-FROM NCSS_Pedon_Taxonomy
-ORDER BY pedon_key;"
-
-## get hz data from KSSL snapshot
-## 2014-09-18 adapted from the "Master Query" ##
-## 2015-12-10 picking up new columns and replacing NULL CaCO3 will 0
-## 2017-04-24 must solve prep code issues before t3 will contain anything useful
+# get hz data from KSSL snapshot
+# 2014-09-18 adapted from the "Master Query"
+# 2015-12-10 picking up new columns and replacing NULL CaCO3 will 0
+# 2017-04-24 must solve prep code issues before t3 will contain anything useful
+# 2017-09-11 now using SQLite version of LIMS snapshot
 q.hz <- "SELECT
 NCSS_Pedon_Taxonomy.pedon_key, NCSS_Layer.labsampnum,
 hzn_top, hzn_bot, hzn_desgn, hzn_desgn_old,
@@ -32,34 +25,34 @@ p_olsn, p_nz,
 db_13b, db_od, COLEws,
 db.wrd_ws13 as whc, db.w3cld, db.w15l2, db.w15cly, cec7_cly, wpg2 as frags,
 t3.wrd_l2
-FROM 
-((((((((
+FROM
 NCSS_Pedon_Taxonomy 
-LEFT OUTER JOIN NCSS_Layer ON NCSS_Pedon_Taxonomy.pedon_key = NCSS_Layer.pedon_key)
-LEFT OUTER JOIN CEC_and_Bases ON NCSS_Layer.labsampnum = CEC_and_Bases.labsampnum) 
-LEFT OUTER JOIN PSDA_and_Rock_Fragments ON NCSS_Layer.labsampnum = PSDA_and_Rock_Fragments.labsampnum)
-LEFT OUTER JOIN Carbon_and_Extractions ON NCSS_Layer.labsampnum = Carbon_and_Extractions.labsampnum)
-LEFT OUTER JOIN ph_and_Carbonates ON NCSS_Layer.labsampnum = ph_and_Carbonates.labsampnum)
-LEFT OUTER JOIN (SELECT * FROM Bulk_Density_and_Moisture WHERE prep_code = 'S') AS db ON NCSS_Layer.labsampnum = db.labsampnum) 
-LEFT OUTER JOIN (SELECT * FROM Supplementary_Tier_3 WHERE prep_code = 'S') AS t3 ON NCSS_Layer.labsampnum = t3.labsampnum)
-LEFT OUTER JOIN Salt ON NCSS_Layer.labsampnum = Salt.labsampnum)
+LEFT OUTER JOIN NCSS_Layer ON NCSS_Pedon_Taxonomy.pedon_key = NCSS_Layer.pedon_key
+LEFT OUTER JOIN CEC_and_Bases ON NCSS_Layer.labsampnum = CEC_and_Bases.labsampnum
+LEFT OUTER JOIN PSDA_and_Rock_Fragments ON NCSS_Layer.labsampnum = PSDA_and_Rock_Fragments.labsampnum
+LEFT OUTER JOIN Carbon_and_Extractions ON NCSS_Layer.labsampnum = Carbon_and_Extractions.labsampnum
+LEFT OUTER JOIN ph_and_Carbonates ON NCSS_Layer.labsampnum = ph_and_Carbonates.labsampnum
+LEFT OUTER JOIN (SELECT * FROM Bulk_Density_and_Moisture WHERE prep_code = 'S') AS db ON NCSS_Layer.labsampnum = db.labsampnum
+LEFT OUTER JOIN (SELECT * FROM Supplementary_Tier_3 WHERE prep_code = 'S') AS t3 ON NCSS_Layer.labsampnum = t3.labsampnum
+LEFT OUTER JOIN Salt ON NCSS_Layer.labsampnum = Salt.labsampnum
 LEFT OUTER JOIN Phosphorus ON NCSS_Layer.labsampnum = Phosphorus.labsampnum
 ORDER BY NCSS_Pedon_Taxonomy.pedon_key, hzn_top ASC;"
 
+# get essential site level records: IDs and PCI
+# site / morph data are later extracted from NASIS data sources
+q.site <- "SELECT pedon_key, pedlabsampnum, pedon_completene AS pedon_completeness_index 
+FROM NCSS_Pedon_Taxonomy 
+ORDER BY pedon_key;"
 
+# setup connection to SQLite DB from FGDB export
+db <- dbConnect(RSQLite::SQLite(), "E:/working_copies/lab-data-delivery/code/text-file-to-sqlite/KSSL-data.sqlite")
 
-# setup connections
-kssl.channel <- odbcConnectAccess('S:/NRCS/Lab_Data/NCSS_Soil_Characterization_Database_09_6_2016.mdb', readOnlyOptimize=TRUE)
-
-# get data from KSSL snapshot
-s <- sqlQuery(kssl.channel, q.site, stringsAsFactors=FALSE)
-h <- sqlQuery(kssl.channel, q.hz, stringsAsFactors=FALSE)
-
-# close connections
-odbcCloseAll()
+# fetch records
+h <- dbGetQuery(db, q.hz)
+s <- dbGetQuery(db, q.site)
 
 # save cached data
 save(s, h, file='S:/NRCS/Lab_Data/cached-data/kssl-site-and-horizon-data.Rda')
 
-
+dbDisconnect(db)
 
