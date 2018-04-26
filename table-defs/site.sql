2c265c8bc4cd19cda2145fb30f837ebb77b04cec
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-04-26 09:42:09 -0700

    moving table defs

---
+++ table-defs/site.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "kssl.site" 
+( "pedon_key" integer,
+	"pedlabsampnum" text,
+	"pedon_id" text,
+	"x" float8,
+	"y" float8,
+	"sampled_as" text,
+	"correlated_as" text,
+	"correlated_taxon_kind" text,
+	"pscs_top" float8,
+	"pscs_bottom" float8,
+	"pedon_completeness_index" integer,
+	"ssl_taxsubgroup" text,
+	"taxonname" text,
+	"state" text,
+	"mlra" text 
+)
